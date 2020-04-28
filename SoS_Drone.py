118c02399c22cd9486917acdc3d7e881fbd40e89
rawinpinkoh <62972809+rawinpinkoh@users.noreply.github.com>
2020-04-28 16:15:36 -0400

    Add files via upload

--- SoS_Drone.py
+++ SoS_Drone.py
@@ -24,6 +24,7 @@ class drone:
         #define origin
         self.xorigin=xcell
         self.yorigin=ycell
+        #Define map size and boundary
         self.mapsize=mapsize
         self.maprange=np.arange(0,mapsize)
 
@@ -35,7 +36,7 @@ class drone:
     #move to random adjacent cell
     def moverandom(self):
         randlist = [[0,1],[1,1],[1,0],[-1,1],[-1,0],[-1,-1],[0,-1],[1,-1]] #possible movement, excluding [0,0] so it wont stay in the same grid
-        movement_step = random.choice(randlist)
+        movement_step = random.choice(randlist) #get random direction from the list
         self.xcell = self.xcell+movement_step[0][0]
         self.ycell = self.ycell+movement_step[0][1]
         
@@ -44,7 +45,7 @@ class drone:
             movement_step = random.choice(randlist)
             self.xcell = self.xcell+movement_step[0][0]
             self.ycell = self.ycell+movement_step[0][1]
-        self.batt_drain
+        self.batt_drain # Drain battery after the move
 
         
 
@@ -72,6 +73,8 @@ class drone:
         #move toward y
         self.ycell=self.ycell+ydir
         self.batt_drain
+        
+        #check if need to return to base
         self.check_return
     
     #check battery level        
@@ -82,8 +85,10 @@ class drone:
     
     #read scan heat
     def scan_heat(self,heatmap):
+        #preallocation the array
         self.fire_location = [[999,999]]
-        #read fire status in IR range
+        
+        #Identify search boundart so it wont spot outside map boundary
         if (self.xcell-self.IR_radius) < 0:
             IR_lower_x = 0
         else:
@@ -103,11 +108,11 @@ class drone:
             IR_upper_y = 99
         else:
             IR_upper_y = self.ycell+self.IR_radius
-            
+           
         self.IR_range_x=np.arange(IR_lower_x,IR_upper_x)
         self.IR_range_y=np.arange(IR_lower_y,IR_upper_y)
-        print(self.IR_range_x)
-        print(self.IR_range_y)
+        
+        #read fire status in IR range
         for x in self.IR_range_x:
             for y in self.IR_range_y:
                 if heatmap[x][y] >= self.IR_heat_trigger:
@@ -116,8 +121,9 @@ class drone:
                     print("and")
                     print(y)
                     self.fire_location = np.append(self.fire_location,[[x,y]],axis = 0)
-
-        #read smoke status in visual range
+        
+        #Identify search boundart so it wont spot outside map boundary
+        
         if (self.xcell-self.Visual_radius) < 0:
             Visual_lower_x = 0
         else:
@@ -140,7 +146,8 @@ class drone:
             
         self.Visual_range_x=np.arange(Visual_lower_x,Visual_upper_x)
         self.Visual_range_y=np.arange(Visual_lower_y,Visual_upper_y)
-
+        
+        #read smoke status in visual range
         for x in self.Visual_range_x:
             for y in self.Visual_range_y:
                 if heatmap[x][y] >= self.Visual_heat_trigger:
@@ -151,7 +158,7 @@ class drone:
                     self.fire_location = np.append(self.fire_location,[[x,y]],axis = 0)
         self.fire_location = np.delete(self.fire_location, np.where(self.fire_location == [999,999]),0)
         return np.unique(self.fire_location, axis = 0)
-        #send alarm
+        #send fire location
 
 
     
